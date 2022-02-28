b76fd461274ef27c7663e4de5b94a2721bed441b
Sariputa Tiong <info@digitalfuture.melbourne>
2022-03-01 10:08:53 +1100

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']; //Driver Arrays
+
+const returnFirstTwoDrivers = ([]) => { return drivers.slice(0,2); } //Arrow Function to return only the first two drivers
+const returnLastTwoDrivers = ([]) => { return drivers.slice(-2) } //Arrow Function to return only the last two drivers
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers] 
+
+const createFareMultiplier = function (fareMultiplier) { //Creating Fare Multiplier
+    return function(multiplier) {
+        return fareMultiplier * multiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (drivers, func) => { //Selecting Different Drivers
+    return func(drivers)
+}
